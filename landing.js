da751b57f0a8cd614678c19c8a9f7f58f3d8665d
Reeve Fernandes <ferna312@msu.edu>
2023-01-21 07:09:21 -0500

    final

---
+++ landing.js
@@ -0,0 +1,9 @@
+var myNav = document.getElementById("nav");
+window.onscroll = function () {
+  if (document.body.scrollTop >= 200) {
+    console.log("yes");
+    myNav.classList.add("nav-solid");
+  } else {
+    myNav.classList.remove("nav-solid");
+  }
+};
